27909b6eb61f2de1e5c3e54de03576392fd214ac
DarkoRockk <shadrin.dev@gmail.com>
2021-01-23 20:33:38 +0300

    4.4. Вложенный оператор if else [#455478]

---
+++ src/ru/job4j/MultiMax.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+public class MultiMax {
+    public static int max(int first, int second, int third) {
+        int result = 0;
+        if (first >= second && first >= third) {
+            result = first;
+        } else {
+            if (second >= first && second >= third) {
+                result = second;
+            } else {
+                result = third;
+            }
+        }
+        return result;
+    }
+}
---
+++ test/ru/job4j/MultiMaxTest.java
@@ -0,0 +1,32 @@
+package ru.job4j;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class MultiMaxTest {
+
+    @Test
+    public void whenSecondMax() {
+        int result = MultiMax.max(1, 4, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenFirstMax() {
+        int result = MultiMax.max(4, 1, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenThirdMax() {
+        int result = MultiMax.max(1, 2, 4);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenAllAreSame() {
+        int result = MultiMax.max(4, 4, 4);
+        assertThat(result, is(4));
+    }
+}
